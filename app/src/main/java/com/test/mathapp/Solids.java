6cdc2d0a0e8f8b64bfbe26ca2bc570e900cddf68
Owais Ali <okbatti@gmail.com>
2020-04-04 12:58:57 +0530

    Add

--- app/src/main/java/com/test/mathapp/Solids.java
+++ app/src/main/java/com/test/mathapp/Solids.java
@@ -5,6 +5,7 @@ import androidx.appcompat.widget.Toolbar;
 import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.content.Intent;
+import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
@@ -23,6 +24,7 @@ public class Solids extends AppCompatActivity {
         drawerLayout = findViewById(R.id.drawer);
         toolbar = findViewById(R.id.toolbar);
         back = findViewById(R.id.back);
+
 
         back.setOnClickListener(new View.OnClickListener() {
             @Override
